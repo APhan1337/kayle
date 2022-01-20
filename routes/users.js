3e5d663a5fd4225bd0998eabd55e517ce28173c4
Alex Phan <avphan97@gmail.com>
2022-01-19 20:03:28 -0800

    Set up route to return Metadata

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,20 @@
+var metaplex = require('@metaplex/js');
+var metadata = require('@metaplex-foundation/mpl-token-metadata');
 var express = require('express');
 var router = express.Router();
 
-var erwin = {
-	"id": 13,
-	"age": null,
-	"photo_id": "https://res.cloudinary.com/erwin-json/image/upload/v1631740732/erwin-smith_dmbc1f.png",
-	"branch": "scouts",
-	"height_cm": 188,
-	"name": {
-		"first": "Erwin",
-		"last": "Smith"
-	},
-	"expeditions": [
-		{
-			"name": "Find Land",
-			"time": "2022-01-18T01:35:03.552Z"
-		}
-	]
+const connection = new metaplex.Connection('devnet');
+const tokenPublicKey = 'Gz3vYbpsB2agTsAwedtvtTkQ1CG9vsioqLW3r9ecNpvZ';
+
+const run = async () => {
+  try {
+    const ownedMetadata = await metadata.Metadata.load(connection, tokenPublicKey);
+    console.log(ownedMetadata);
+  } catch {
+    console.log('Failed to fetch metadata');
+  }
 };
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-	let data = erwin;
-  	res.json(data);
-});
-
-/* UPDATE user */
-/* CREATE user */
-/* DELETE user */
+run();
 
 module.exports = router;
