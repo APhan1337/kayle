92054bfb1c9439571a5352bb3563970b60bba6db
Alex Phan <avphan97@gmail.com>
2022-01-19 22:46:34 -0800

    Add metadata endpoint logic using query string

--- routes/users.js
+++ routes/users.js
@@ -4,17 +4,28 @@ var express = require('express');
 var router = express.Router();
 
 const connection = new metaplex.Connection('devnet');
-const tokenPublicKey = 'Gz3vYbpsB2agTsAwedtvtTkQ1CG9vsioqLW3r9ecNpvZ';
+// const tokenPublicKey = 'Gz3vYbpsB2agTsAwedtvtTkQ1CG9vsioqLW3r9ecNpvZ';
 
-const run = async () => {
-  try {
-    const ownedMetadata = await metadata.Metadata.load(connection, tokenPublicKey);
-    console.log(ownedMetadata);
-  } catch {
-    console.log('Failed to fetch metadata');
-  }
-};
 
-run();
+
+router.get(['/', '/:metadata'], function(req, res, next) {
+	console.log(JSON.stringify(req.query));
+	var tokenPublicKey = req.query.tokenPublicKey;
+
+	const run = async () => {
+		console.log("Running...");
+		console.log(tokenPublicKey);
+		try {
+			const ownedMetadata = await metadata.Metadata.load(connection, tokenPublicKey);
+			console.log(ownedMetadata);
+			res.json(ownedMetadata);
+		  } catch {
+			console.log('Failed to fetch metadata');
+			res.render('error');
+		  }
+	};
+
+	run();
+});
 
 module.exports = router;
