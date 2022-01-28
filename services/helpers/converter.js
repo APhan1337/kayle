2f83965cf3109a2c1eb8d44d64e44b2abf874e06
Kevin Ngo <kngo1207@verizon.net>
2022-01-27 17:57:44 -0800

    Added helper section

---
+++ services/helpers/converter.js
@@ -0,0 +1,11 @@
+const conversionFactor = 0.000000001; // {sol} * conversionFactor = # of lamports
+
+function convertLamportToSol(lamports) {
+  return null;
+}
+
+function convertSolToLamport(sol) {
+  return null;
+}
+
+module.exports = { convertLamportToSol, convertSolToLamport };
