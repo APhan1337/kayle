7a4223831b8705aa991954abaa3cc18a1da94b3c
Alex Phan <avphan97@gmail.com>
2022-03-02 13:13:13 -0800

    Add skeleton header and light/dark mode switch

--- kayle-web/src/App.js
+++ kayle-web/src/App.js
@@ -1,13 +1,18 @@
 //import "./App.css";
+import openAPIspec from "./swagger.base.json";
+import Header from "./components/header/Header.js";
 
 import { RedocStandalone } from "redoc";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <RedocStandalone specUrl="https://kayle-blockchain.herokuapp.com/swagger.base.json" />
-      </header>
+      <Header />
+      <div className="App-content">
+        {/* <RedocStandalone specUrl="https://kayle-blockchain.herokuapp.com/swagger.base.json" /> */}
+        {/* <RedocStandalone specUrl="http://localhost:3000/swagger.base.json" /> */}
+        <RedocStandalone spec={openAPIspec} />
+      </div>
     </div>
   );
 }
---
+++ kayle-web/src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <section clalssName="header">
+      <section className="header-logo">
+        <p>Kayle</p>
+      </section>
+      <section className="header-switch">
+        <input type="checkbox"></input>
+      </section>
+    </section>
+  );
+}
+
+export default Header;
